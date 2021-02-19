b4261f6a70801dffff3727fe77b05920f5a65d4e
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2021-02-19 20:55:23 +0100

    add redux to application

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ExchangeRate } from "./components/ExchangeRate";
 import "./style.css";
+import { Provider } from 'react-redux'
+import { store } from './store';
 
-ReactDOM.render(<ExchangeRate />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider>
+        <ExchangeRate store={store} />
+    </Provider>,
+    document.getElementById("root"));
